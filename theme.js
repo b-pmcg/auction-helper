3fde24cc7c1057e336f7c823463727fda89fd3b0
Adrian le Bas <adrianleb@gmail.com>
2020-03-14 16:45:33 +0100

    theme updates

--- theme.js
+++ theme.js
@@ -14,7 +14,7 @@ export default {
     monospace: 'Menlo, monospace',
   },
 
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontSizes: [11, 13, 14, 15, 16, 18, 20, 24, 32, 48, 64, 96],
 
   fontWeights: {
     body: 400,
@@ -31,6 +31,7 @@ export default {
     text: '#231536',
     background: '#F6F8F9',
     primary: '#1AAB9B',
+    blackThree: '#333'
 
     // text: '#000',
     // background: '#fff',
@@ -62,9 +63,23 @@ export default {
       textTransform: 'uppercase',
       fontWeight: 'semiBold',
     },
+    h2: {
+      fontSize: 6,
+      fontWeight: 'semiBold',
+      letterSpacing: '0.4px',
+      
+    },
     small: {
       fontSize: 1,
       color: 'red'
+    }
+  },
+  links: {
+    nav: {
+        fontSize: 5,
+        fontWeight: 'body',
+        letterSpacing: '0.4px',
+        color: 'blackThree'
     }
   },
   styles: {
@@ -74,53 +89,54 @@ export default {
       lineHeight: 'body',
       fontWeight: 'body',
     },
-    h1: {
-      variant: 'text.heading',
-      fontSize: 5,
-    },
-    h2: {
-      variant: 'text.heading',
-      fontSize: 4,
-    },
-    h3: {
-      variant: 'text.heading',
-      fontSize: 3,
-    },
-    h4: {
-      variant: 'text.heading',
-      fontSize: 2,
-    },
-    h5: {
-      variant: 'text.heading',
-      fontSize: 1,
-    },
-    h6: {
-      variant: 'text.heading',
-      fontSize: 0,
-    },
-    pre: {
-      fontFamily: 'monospace',
-      overflowX: 'auto',
-      code: {
-        color: 'inherit',
-      },
-    },
-    code: {
-      fontFamily: 'monospace',
-      fontSize: 'inherit',
-    },
-    table: {
-      width: '100%',
-      borderCollapse: 'separate',
-      borderSpacing: 0,
-    },
-    th: {
-      textAlign: 'left',
-      borderBottomStyle: 'solid',
-    },
-    td: {
-      textAlign: 'left',
-      borderBottomStyle: 'solid',
-    },
+    // h1: {
+    //   variant: 'text.heading',
+    //   fontSize: 7,
+    // },
+    // h2: {
+    //   variant: 'text.heading',
+    //   fontSize: 6,
+    //   fontWeight: 'body'
+    // },
+    // h3: {
+    //   variant: 'text.heading',
+    //   fontSize: 5,
+    // },
+    // h4: {
+    //   variant: 'text.heading',
+    //   fontSize: 4,
+    // },
+    // h5: {
+    //   variant: 'text.heading',
+    //   fontSize: 3,
+    // },
+    // h6: {
+    //   variant: 'text.heading',
+    //   fontSize: 2,
+    // },
+    // pre: {
+    //   fontFamily: 'monospace',
+    //   overflowX: 'auto',
+    //   code: {
+    //     color: 'inherit',
+    //   },
+    // },
+    // code: {
+    //   fontFamily: 'monospace',
+    //   fontSize: 'inherit',
+    // },
+    // table: {
+    //   width: '100%',
+    //   borderCollapse: 'separate',
+    //   borderSpacing: 0,
+    // },
+    // th: {
+    //   textAlign: 'left',
+    //   borderBottomStyle: 'solid',
+    // },
+    // td: {
+    //   textAlign: 'left',
+    //   borderBottomStyle: 'solid',
+    // },
   },
 }
