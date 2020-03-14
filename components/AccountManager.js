08fcaa5fb95c0993648ce828cae51c5ab8936fe2
Phil Bain <bain.pmcg@gmail.com>
2020-03-14 21:13:34 +0100

    check for allowance in acct mgr

--- components/AccountManager.js
+++ components/AccountManager.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import useMaker from '../hooks/useMaker';
 import {
   Heading,
@@ -15,54 +15,58 @@ import {
 } from 'theme-ui';
 import { AUCTION_DATA_FETCHER } from '../constants';
 
+const REQUIRED_ALLOWANCE = 0;
+
 export default ({ web3Connected }) => {
   const { maker } = useMaker();
   const [daiApprovePending, setDaiApprovePending] = useState(false);
-  const [proxyDetails, setProxyDetails] = useState({});
+  const [hopeApprovePending, setHopeApprovePending] = useState(false);
+  const [hasAllowance, setHasAllowance] = useState(false);
+  const [joinAddress, setJoinAddress] = useState('');
+
+  useEffect(() => {
+    if (web3Connected) {
+      (async () => {
+        const joinDaiAdapterAddress = maker
+          .service('smartContract')
+          .getContractByName('MCD_JOIN_DAI').address;
+        setJoinAddress(joinDaiAdapterAddress);
+        const allowance = await maker
+          .getToken('MDAI')
+          .allowance(maker.currentAddress(), joinDaiAdapterAddress);
+        setHasAllowance(allowance.gt(REQUIRED_ALLOWANCE) ? true : false);
+      })();
+    }
+  }, [maker, web3Connected]);
 
   const giveDaiAllowance = async address => {
     setDaiApprovePending(true);
     try {
       await maker.getToken('MDAI').approveUnlimited(address);
-      setProxyDetails(proxyDetails => ({
-        ...proxyDetails,
-        hasDaiAllowance: true
-      }));
+      setHasAllowance(true);
     } catch (err) {
       const message = err.message ? err.message : err;
       const errMsg = `unlock dai tx failed ${message}`;
       console.error(errMsg);
-      // addToastWithTimeout(errMsg, dispatch);
     }
     setDaiApprovePending(false);
   };
 
   const giveHope = async address => {
-    await maker
-      .service('smartContract')
-      .getContract('MCD_VAT')
-      .hope(address);
+    setHopePending(true);
+    try {
+      await maker
+        .service('smartContract')
+        .getContract('MCD_VAT')
+        .hope(address);
+    } catch (err) {
+      const message = err.message ? err.message : err;
+      const errMsg = `hope tx failed ${message}`;
+      console.error(errMsg);
+    }
+    setHopeApprovePending(false);
   };
-  if (!web3Connected) {
-    return (
-      <Flex
-        sx={{
-          justifyContent: 'center',
-          py: 4
-        }}
-      >
-        <Heading
-          as="h2"
-          variant="h2"
-          sx={{
-            mb: 2
-          }}
-        >
-          To participate in auctions, connect your wallet
-        </Heading>
-      </Flex>
-    );
-  }
+
   return (
     <Box
       sx={{
@@ -88,23 +92,24 @@ export default ({ web3Connected }) => {
           mx: 'auto'
         }}
       >
-        <Button onClick={() => giveDaiAllowance(maker.currentAddress())}>
-          Unlock Dai in your wallet
+        <Button
+          onClick={() => giveDaiAllowance(joinAddress)}
+          disabled={!web3Connected || hasAllowance}
+        >
+          {hasAllowance ? 'Dai Unlocked' : 'Unlock Dai in your wallet'}
         </Button>
         <Button
           onClick={() => {
-            const service = maker.service(AUCTION_DATA_FETCHER);
-            giveHope(service.flipEthAddress);
+            const flipEthAddress = maker
+              .service('smartContract')
+              .getContractByName('MCD_FLIP_ETH_A').address;
+            giveHope(flipEthAddress);
           }}
+          disabled={!web3Connected}
         >
           Unlock Dai in the adapter
         </Button>
-        <Button
-          onClick={() => {
-            const service = maker.service(AUCTION_DATA_FETCHER);
-            service.joinDaiAdapterAddressZ;
-          }}
-        >
+        <Button onClick={() => giveHope(joinAddress)} disabled={!web3Connected}>
           Unlock Dai in the VAT
         </Button>
       </Grid>
