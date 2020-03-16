bf74e7facedaf6edb9c1eeed99f1bebdcbcf5ab4
Phil Bain <bain.pmcg@gmail.com>
2020-03-16 12:22:38 +0100

    call .toNumber() on bignumber

--- hooks/useAuctionActions.js
+++ hooks/useAuctionActions.js
@@ -6,15 +6,15 @@ const useAuctionActions = () => {
   const { maker, web3Connected } = useMaker();
 
   async function callTend(auctionId, lotSize, bidAmount) {
+    console.log('auctionId, lotSize, bidAmount', auctionId, lotSize, bidAmount);
     try {
       const t = await maker
         .service('validator')
-        .tend(auctionId, lotSize, bidAmount);
+        .tend(auctionId, lotSize, bidAmount.toNumber());
     } catch (err) {
       window.alert(err);
     }
   }
-
 
   return { callTend };
 };
