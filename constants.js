ccbc54d02798682298a65e46d3d4d43f8198c185
Adrian le Bas <adrianleb@gmail.com>
2020-03-19 14:27:10 +0100

    add constant

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-import BigNumber from "bignumber.js";
+import BigNumber from 'bignumber.js';
 
 export const AUCTION_DATA_FETCHER = 'validator'; //TODO update this when we change the name
 
@@ -13,13 +13,13 @@ export const TX_PENDING = 'pending';
 export const TX_SUCCESS = 'success';
 export const TX_ERROR = 'error';
 
+export const REFETCH_BLOCK_INTERVAL = 5;
 
 export const balanceRounding = {
   zeroToOne: 6,
   oneTo10K: 2,
   over10K: 0
 };
-
 
 // Numbers
 export const ZERO = new BigNumber(0);
